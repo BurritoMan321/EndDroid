e506ceb1d372ff5b2286d3aa8a2d9513be67c046
XenloX <49673580+BurritoMan321@users.noreply.github.com>
2019-05-14 13:17:11 +0200

    changes

--- Code/Socket.py
+++ Code/Socket.py
@@ -1,10 +1,13 @@
 import socket
+host = '10.25.222.125'
+port = 8080
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost', 8888))
+s.bind((host, port))
 s.listen(1)
 conn, addr = s.accept()
 while 1:
     data = conn.recv(1024)
+    print(data)
     if not data:
         break
     conn.sendall(data)
--- Code/client_socket.py
+++ Code/client_socket.py
@@ -1,7 +1,7 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 8888))
-s.sendall('Hello, world')
+s.connect(('10.25.222.125', 8080))
+s.sendall(b'Hello, world')
 data = s.recv(1024)
 s.close()
 print ("Received"), repr(data)
